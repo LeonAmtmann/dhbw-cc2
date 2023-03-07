4c8f421fd00e0d79eb2cf5bec527ac94b0c7adc5
Leon Amtmann <35031775+LeonAmtmann@users.noreply.github.com>
2023-03-07 20:06:04 +0100

    Added missing variables to declaration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,23 @@ variable "admin_password" {
   type = string
   default = ""
 }
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
