6b81b582794c27e1636514db8a29b030e77b8bdd
Leon Amtmann <35031775+LeonAmtmann@users.noreply.github.com>
2023-03-07 20:09:55 +0100

    Removed duplicate variable declatations LOL

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,3 @@
-variable "client_id" {
-  type = string
-  default = ""
-}
-
-variable "client_secret" {
-  type = string
-  default = ""
-}
-
 variable "admin_username" {
   type = string
   default = ""
@@ -16,10 +6,6 @@ variable "admin_username" {
 variable "admin_password" {
   type = string
   default = ""
-}
-
-variable "tenant_id" {
-  type = string
 }
 
 variable "subscription_id" {
