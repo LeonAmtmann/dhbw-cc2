9acf4c6c230029a69c4f563108b5f5480309d800
Leon Amtmann <35031775+LeonAmtmann@users.noreply.github.com>
2023-03-07 20:16:32 +0100

    Updated provider configuration features

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,12 @@
 provider "azurerm" {
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion = false
+    }
+    network {
+      relaxed_locking = true
+    }
+  }
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
