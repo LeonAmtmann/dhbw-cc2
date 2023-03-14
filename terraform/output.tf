3427100e5b93eda4e8a04c95809a6903479415ee
LeonAmtmann <github@amtmann.de>
2023-03-14 20:55:00 +0100

    Removed old terraform files

--- terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip_address" {
-  value = azurerm_network_interface.dhbw-cc2-networkinterface.ip_configuration
-}
--- terraform/resource-group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "dhbw-cc2-group" {
-  name     = "dhbw-cc2-group"
-  location = "westeurope"
-}
